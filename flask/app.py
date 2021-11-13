fca4c32418f6a455f4c36569beda9f30202be270
Dovydas Martinkus <dovydas.martinkus@gmail.com>
2021-11-13 14:17:50 +0200

    Data

--- flask/app.py
+++ flask/app.py
@@ -22,3 +22,42 @@ def kpi():
     df=df.loc[:,["city","rank"]]
     df_dict = df.to_dict("list")
     return json.dumps(df_dict)
+
+
+
+
+@app.route("/tersalai")
+def tersalai():
+    df=pd.read_pickle("../tersalai.pkl")
+    city = None
+    category = None
+    year = None
+    
+    try:
+        city=request.args.get("city")
+    except ValueError:
+        pass
+    try:
+        category=request.args.get("category")
+    except ValueError:
+        pass
+    try:
+        year=request.args.get("year")
+    except ValueError:
+        pass
+        
+    category_lookup = {"co2": "Anglies monoksidas, tonos",
+                       "azotas":"Azoto oksidai, tonos",
+                       "visi":"Visi teršalai"}
+    
+    if category is not None:
+        df = df[df["Teršalai"]==category_lookup[category]]
+    if city is not None:
+        df = df[df["Administracinė teritorija"]==city]
+    if year is not None:
+        df = df[df["Laikotarpis"]==year]
+    
+
+
+    df_dict = df.to_dict("list")
+    return json.dumps(df_dict)
