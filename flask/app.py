8b60469c58b00657bcaea07595fedf708b90a278
Dovydas Martinkus <dovydas.martinkus@gmail.com>
2021-11-13 10:44:01 +0200

    Small error in the data

--- flask/app.py
+++ flask/app.py
@@ -4,6 +4,8 @@ import json
 
 app = Flask(__name__)
 
+cities = ["Vilnius","Kaunas","Alytus","Šiauliai","Marijampolė","Klaipeda","Siauliai","Panevezys"]
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
@@ -12,7 +14,11 @@ def hello_world():
 @app.route("/kpi")
 def kpi():
     df=pd.read_pickle("../kpi_category_ranks.pkl")
-    #query=request.args.get("category")
-    #return query
+    query=request.args.get("category")
+    query_split = query.split("-")
+    df["rank"]=df.loc[:,query_split].apply(sum,axis=1)
+    df["rank"]=df["rank"].rank()
+    df = df[df["city"].isin(cities)]
+    df=df.loc[:,["city","rank"]]
     df_dict = df.to_dict("list")
     return json.dumps(df_dict)
