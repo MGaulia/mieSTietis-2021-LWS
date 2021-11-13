79f91f434ee7190c6890431be1d7d024fe9d9d81
Matas Gaulia <39590298+MGaulia@users.noreply.github.com>
2021-11-13 16:46:58 +0200

    Add files via upload

--- nuotekos.py
+++ nuotekos.py
@@ -3,20 +3,23 @@ from flask_restful import Resource, Api
 from flask_cors import CORS
 import pandas as pd
 
+def form_json_by_city(filepath):
+    nuotekos = pd.read_csv(filepath)
+    result = {}
+    for city in set(nuotekos["city"]):
+        temp = nuotekos[nuotekos["city"] == city]
+        result[city] = [{"x":x, "y":y} for x,y in zip(temp.x, temp.y)]
+
+    return result
+
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
 
-nuotekos = pd.read_csv("kpi/nuotekos.csv")
-
-result_nuotekos = {}
-for city in set(nuotekos["city"]):
-    temp = nuotekos[nuotekos["city"] == city]
-    result_nuotekos[city] = [{"x":x, "y":y} for x,y in zip(temp.x, temp.y)]
 
 class nuotekos(Resource):
     def get(self):
-        return result_nuotekos
+        return form_json_by_city("kpi/nuotekos.csv")
 
 api.add_resource(nuotekos, '/nuotekos')
 
