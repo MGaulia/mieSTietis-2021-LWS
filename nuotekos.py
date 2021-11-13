2d6f2b97e221b9042406e0f9b4bcca5ac8ed7f6d
Matas Gaulia <39590298+MGaulia@users.noreply.github.com>
2021-11-13 15:50:14 +0200

    Add files via upload

--- nuotekos.py
+++ nuotekos.py
@@ -3,28 +3,22 @@ from flask_restful import Resource, Api
 from flask_cors import CORS
 import pandas as pd
 
-
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
 
-nuotekos = pd.read_csv("csv/nuotekos.csv")
-#nuotekos = pd.read_pickle('/csv/nuoteku_isvalimas.pkl.csv')
-nuotekos = nuotekos.drop(['Rodiklis', "Matavimo vienetai"], 1)
-nuotekos = nuotekos.replace("Sostinės regionas", "Vilniaus apskiritis")
-nuotekos = pd.merge(
-    nuotekos[nuotekos["Išvalymas"] == "Išleista išvalytų iki normos nuotekų"],
-    nuotekos[nuotekos["Išvalymas"] == "Iš viso išleista nuotekų"],
-    on=["Administracinė teritorija", "Laikotarpis"])
-nuotekos["y"] = nuotekos["Reikšmė_y"]/nuotekos["Reikšmė_x"]
-nuotekos = nuotekos.drop(['Išvalymas_x', "Išvalymas_y", "Reikšmė_x", "Reikšmė_y"], 1)
-nuotekos = nuotekos.rename({'Laikotarpis': 'x', 'Administracinė teritorija': 'city'}, axis=1)
+nuotekos = pd.read_csv("kpi/nuotekos.csv")
 
-class population_by_cities(Resource):
+result_nuotekos = {}
+for city in set(nuotekos["city"]):
+    temp = nuotekos[nuotekos["city"] == city]
+    result_nuotekos[city] = [{"x":x, "y":y} for x,y in zip(temp.x, temp.y)]
+
+class nuotekos(Resource):
     def get(self):
-        return jsonify(nuotekos.to_dict(orient="records"))
+        return result_nuotekos
 
-api.add_resource(population_by_cities, '/nuotekos')  # www.linktoapi.com/populations
+api.add_resource(nuotekos, '/nuotekos')
 
 if __name__ == '__main__':
     app.run(debug = True)
